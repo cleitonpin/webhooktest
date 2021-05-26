54f085c832e6a5753adb61b518e63e612aea59e5
cleitonpin <cleiton.aguiar@bunnieshole.com.br>
2021-05-26 15:26:26 -0300

    ff
    Signed-off-by: cleitonpin <cleiton.aguiar@bunnieshole.com.br>

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const Payment = require('./controllers/Payment');
 
 const server = express();
 
+const PORT = process.env.PORT || 3000;
+
 server.post('/webhook', Payment.webhook);
 
-server.listen(3001 || process.env.PORT, () => console.log('rodando'))
+server.listen(PORT, () => console.log('rodando'))
